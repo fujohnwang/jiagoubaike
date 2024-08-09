9d45eaaec58b6bb73a6163925347dc337e04fb1c
afoo <i@afoo.me>
2024-08-09 17:42:55 +0800

    add page find to jiagoubaike

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   site: 'https://jiagoubaike.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(),pagefind()],
   trailingSlash: 'never',
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
